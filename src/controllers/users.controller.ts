8e210d0971e1077aabfb02f471c060c859537553
DESKTOP-9RJEA40\Srun Kerry <srunkerry@gmail.com>
2026-06-01 16:36:03 +0700

    add update profile function

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
 import { IUser, userModel } from "../models/users";
-import { uploadToCloudinary } from "../config/cloudinary";
+import { deleteFromCloudinary, uploadToCloudinary } from "../config/cloudinary";
 import bcrpyt from "bcrypt";
 export const userController = {
+  getMany: async (req: Request, res: Response) => {
+    try {
+      const users = await userModel.find();
+      res.json(users);
+    } catch (e) {
+      console.log(e);
+    }
+  },
   create: async (req: Request, res: Response) => {
     try {
       const { url, publicId } = await uploadToCloudinary(
@@ -19,6 +27,33 @@ export const userController = {
       };
       await userModel.create(user);
       res.status(201).json(user);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  update: async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id;
+      const user = await userModel.findById(id);
+      if (!user) return res.status(404).json({ message: "User not found" });
+      if (req.file) {
+        if (user.profilePublicId) {
+          await deleteFromCloudinary(user.profilePublicId);
+        }
+        const { url, publicId } = await uploadToCloudinary(
+          req.file.buffer,
+          "profiles",
+        );
+        user.profile = url;
+        user.profilePublicId = publicId;
+      }
+      if (req.body.password) {
+        const salt = await bcrpyt.genSalt();
+        user.password = await bcrpyt.hash(req.body.password, salt);
+      }
+      user.username = req.body.username ?? user.username;
+      await user.save();
+      res.status(200).json(user);
     } catch (e) {
       console.log(e);
     }
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -2,8 +2,7 @@ import { Request, Response, Router } from "express";
 import { upload } from "../config/cloudinary";
 import { userController } from "../controllers/users.controller";
 const routers = Router();
-routers.get("/", async (req: Request, res: Response) => {
-  res.json({ msg: "test route" });
-});
+routers.get("/", userController.getMany);
 routers.post("/", upload.single("profile"), userController.create);
+routers.patch("/:id", upload.single("profile"), userController.update);
 export const userRoute = routers;
