30917bc54b18ea47cbcd4e05db3642b1e6864930
DESKTOP-9RJEA40\Srun Kerry <srunkerry@gmail.com>
2026-06-02 11:26:04 +0700

    improve db connection prevent connection loop

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-export const connect = () => {
-  mongoose
-    .connect(process.env.MONGO_URI!)
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.error(err));
-};
+let isConnected = false;
+
+export async function connect() {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI!);
+
+    isConnected = db.connections[0]!.readyState === 1;
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    throw error;
+  }
+}
