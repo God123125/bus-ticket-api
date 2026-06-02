4e2e1cec6a9b5993c01b1263a5ff8944a196518d
DESKTOP-9RJEA40\Srun Kerry <srunkerry@gmail.com>
2026-06-02 15:27:31 +0700

    add create function

--- src/controllers/buses.controller.ts
+++ src/controllers/buses.controller.ts
@@ -1,1 +1,17 @@
-import { busModel } from "../models/bus";
+import { Request, Response } from "express";
+import { busModel, IBus } from "../models/bus";
+import { responseServeError } from "../utils/log.util";
+export const busController = {
+  create: async (req: Request, res: Response) => {
+    try {
+      const body: IBus = req.body;
+      const createdData = busModel.create(body);
+      res.status(200).json({
+        msg: "Bus created successfully!",
+        data: createdData,
+      });
+    } catch (e: any) {
+      responseServeError(res, e);
+    }
+  },
+};
