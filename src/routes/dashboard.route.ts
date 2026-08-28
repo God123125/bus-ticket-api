6620f077305720928366116a5e34c1c0eb9fb6ac
DESKTOP-9RJEA40\Srun Kerry <srunkerry@gmail.com>
2026-08-28 13:28:42 +0700

    change route style to make it work with token

--- src/routes/dashboard.route.ts
+++ src/routes/dashboard.route.ts
@@ -1,22 +1,34 @@
-import { Router } from "express";
+// src/routes/dashboard.route.ts
+import { IRoute } from "../interfaces/route";
+import { parseToExpressRoute } from "../utils/route.util";
+import { RoleEnum } from "../interfaces/role-enum";
 import { merchantDashboardController } from "../controllers/dashboard.controller";
-const routes = Router();
 
-routes.get(
-  "/get-merchant-dashboard",
-  merchantDashboardController.get_merchant_dashboard,
-);
-routes.get(
-  "/trends-analytical-chart",
-  merchantDashboardController.trends_analytical_chart,
-);
-routes.get(
-  "/top-performance-destination",
-  merchantDashboardController.top_performance_destination,
-);
-routes.get(
-  "/five-recent-booking",
-  merchantDashboardController.five_recent_booking,
-);
+const routes: IRoute[] = [
+  {
+    path: "/get-merchant-dashboard",
+    method: "get",
+    roles: [RoleEnum.Merchant, RoleEnum.Staff, RoleEnum.Admin],
+    handler: merchantDashboardController.get_merchant_dashboard,
+  },
+  {
+    path: "/trends-analytical-chart",
+    method: "get",
+    roles: [RoleEnum.Merchant, RoleEnum.Staff, RoleEnum.Admin],
+    handler: merchantDashboardController.trends_analytical_chart,
+  },
+  {
+    path: "/top-performance-destination",
+    method: "get",
+    roles: [RoleEnum.Merchant, RoleEnum.Staff, RoleEnum.Admin],
+    handler: merchantDashboardController.top_performance_destination,
+  },
+  {
+    path: "/five-recent-booking",
+    method: "get",
+    roles: [RoleEnum.Merchant, RoleEnum.Staff, RoleEnum.Admin],
+    handler: merchantDashboardController.five_recent_booking,
+  },
+];
 
-export const dashboardRoute = routes;
+export const dashboardRoute = parseToExpressRoute(routes);
